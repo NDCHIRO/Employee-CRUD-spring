49d643c778aadb733d2e9886ec25e39e151fa7f4
amosalah <amrmohamed81298@gmail.com>
2023-07-11 14:59:52 +0300

    added JpaRepository instead of using DAO (@Repository)

--- cruddemo/src/main/java/com/luv2code/springboot/cruddemo/service/EmployeeServiceImp.java
+++ cruddemo/src/main/java/com/luv2code/springboot/cruddemo/service/EmployeeServiceImp.java
@@ -1,41 +1,51 @@
 package com.luv2code.springboot.cruddemo.service;
 
-import com.luv2code.springboot.cruddemo.dao.EmployeeDAO;
+import com.luv2code.springboot.cruddemo.dao.EmployeeRepository;
 import com.luv2code.springboot.cruddemo.entity.Employee;
 import jakarta.transaction.Transactional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
+
 @Service
 public class EmployeeServiceImp implements EmployeeService{
-    private EmployeeDAO employeeDAO;
+    private EmployeeRepository employeeRepository;
     @Autowired
-    public EmployeeServiceImp(EmployeeDAO employeeDAO) {
-        this.employeeDAO = employeeDAO;
+    public EmployeeServiceImp(EmployeeRepository theEmployeeRepository) {
+        this.employeeRepository = theEmployeeRepository;
     }
     @Override
     public List<Employee> findAll() {
-        return employeeDAO.findAll();
+        return employeeRepository.findAll();
     }
 
     @Override
     public Employee findById(int employeeId) {
-        return employeeDAO.findById(employeeId);
+        Optional<Employee> result = employeeRepository.findById(employeeId);
+
+        Employee employee = null;
+        if(result.isPresent())      //it means if result is not null
+            employee=result.get();
+
+        else    // if result is      null
+            throw new RuntimeException("Employee doesn't exist with id "+employeeId);
+
+        return employee;
 
     }
 
     //as we are modifying in the db we have to put @Transactional annotation
-    @Transactional
+
     @Override
     public Employee save(Employee theEmployee) {
-        return employeeDAO.save(theEmployee);
+        return employeeRepository.save(theEmployee);
     }
 
     //as we are modifying in the db we have to put @Transactional annotation
-    @Transactional
     @Override
     public void deleteById(int employeeId) {
-        employeeDAO.deleteById(employeeId);
+        employeeRepository.deleteById(employeeId);
     }
 }
